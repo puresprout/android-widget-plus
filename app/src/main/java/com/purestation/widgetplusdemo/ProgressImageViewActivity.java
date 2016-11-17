bd62fe45610c8b74922b16d8fd6895abadbd56ed
SungHyun Park <puresprout@gmail.com>
2016-11-17 11:01:29 +0900

    progress image view demo improved. added images

--- app/src/main/java/com/purestation/widgetplusdemo/ProgressImageViewActivity.java
+++ app/src/main/java/com/purestation/widgetplusdemo/ProgressImageViewActivity.java
@@ -1,6 +1,7 @@
 package com.purestation.widgetplusdemo;
 
 import android.graphics.Color;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
@@ -8,20 +9,20 @@ import android.view.View;
 import com.purestation.widgetplus.image.ProgressImageView;
 
 public class ProgressImageViewActivity extends AppCompatActivity {
-    private int progress = 0;
+    private ProgressImageView imageView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_progress_image_view);
 
-        final ProgressImageView imageView = (ProgressImageView) findViewById(R.id.progressImageView);
+        imageView = (ProgressImageView) findViewById(R.id.progressImageView);
         imageView.setColor(Color.argb(128, 255, 255, 255));
 
         findViewById(R.id.button).setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                imageView.setProgress(++progress);
+                new ProgressTask().execute(100);
             }
         });
 
@@ -42,4 +43,24 @@ public class ProgressImageViewActivity extends AppCompatActivity {
 //        setContentView(imageView);
     }
 
+    class ProgressTask extends AsyncTask<Integer, Integer, Void> {
+        @Override
+        protected Void doInBackground(Integer... integers) {
+            for (int i = 0; i <= integers[0]; i++) {
+                try {
+                    Thread.sleep(100);
+                    publishProgress(i);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+
+            return null;
+        }
+
+        @Override
+        protected void onProgressUpdate(Integer... values) {
+            imageView.setProgress(values[0]);
+        }
+    }
 }
