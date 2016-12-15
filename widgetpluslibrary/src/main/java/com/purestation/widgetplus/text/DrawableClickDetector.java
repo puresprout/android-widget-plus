f0c82dcae944c7d224013ac001aa82e4a7bb667a
SungHyun Park <puresprout@gmail.com>
2016-12-15 10:44:09 +0900

    ClickableDrawableTextView, ClickableDrawableEditText touch login improved

--- widgetpluslibrary/src/main/java/com/purestation/widgetplus/text/DrawableClickDetector.java
+++ widgetpluslibrary/src/main/java/com/purestation/widgetplus/text/DrawableClickDetector.java
@@ -6,9 +6,11 @@ import android.view.MotionEvent;
 import android.widget.TextView;
 
 public class DrawableClickDetector {
+    private static final int NONE = -1;
+
     private TextView mTextView;
 
-    private Integer mWhichDrawableIsDectecting;
+    private int mTouchingDrawable = NONE;
 
     public DrawableClickDetector(TextView textView) {
         this.mTextView = textView;
@@ -20,61 +22,65 @@ public class DrawableClickDetector {
 
     public boolean detect(MotionEvent event) {
         if (event.getAction() == MotionEvent.ACTION_DOWN) {
-            int x = (int) event.getX();
-            int y = (int) event.getY();
-
-            Drawable[] drawables = mTextView.getCompoundDrawables();
-
-            for (int i = 0; i < drawables.length; i++) {
-                Drawable drawable = drawables[i];
-
-                if (drawable != null) {
-                    Rect rect = drawable.getBounds();
-
-                    if (i == 0) {
-                        if (x >= mTextView.getPaddingLeft() && x <= rect.width() + mTextView.getPaddingLeft() &&
-                                y >= (mTextView.getHeight() / 2) - (rect.height() / 2) && y <= (mTextView.getHeight() / 2) + (rect.height() / 2)) {
-                            mWhichDrawableIsDectecting = i;
-                            return true;
-                        }
-                    } else if (i == 1) {
-                        if (y >= mTextView.getPaddingTop() && y <= rect.height() + mTextView.getPaddingTop() &&
-                                x >= (mTextView.getWidth() / 2) - (rect.width() / 2) && x <= (mTextView.getWidth() / 2) + (rect.width() / 2)) {
-                            mWhichDrawableIsDectecting = i;
-                            return true;
-                        }
-                    } else if (i == 2) {
-                        if (x >= mTextView.getWidth() - rect.width() - mTextView.getPaddingRight() && x <= mTextView.getWidth() - mTextView.getPaddingRight() &&
-                                y >= (mTextView.getHeight() / 2) - (rect.height() / 2) && y <= (mTextView.getHeight() / 2) + (rect.height() / 2)) {
-                            mWhichDrawableIsDectecting = i;
-                            return true;
-                        }
-                    } else if (i == 3) {
-                        if (y >= mTextView.getHeight() - rect.height() - mTextView.getPaddingBottom() && y <= mTextView.getHeight() - mTextView.getPaddingBottom() &&
-                                x >= (mTextView.getWidth() / 2) - (rect.width() / 2) && x <= (mTextView.getWidth() / 2) + (rect.width() / 2)) {
-                            mWhichDrawableIsDectecting = i;
-                            return true;
-                        }
-                    }
-                }
+            mTouchingDrawable = findTouchingDrawable(event);
+            if (mTouchingDrawable != NONE) {
+                return true;
             }
+        } else if (event.getAction() == MotionEvent.ACTION_MOVE) {
         } else if (event.getAction() == MotionEvent.ACTION_UP) {
-            if (mWhichDrawableIsDectecting != null) {
-                ((DrawableClickable) mTextView).performDrawableClick(mWhichDrawableIsDectecting);
-                mWhichDrawableIsDectecting = null;
+            if (mTouchingDrawable != NONE) {
+                if (mTouchingDrawable == findTouchingDrawable(event)) {
+                    ((DrawableClickable) mTextView).performDrawableClick(mTouchingDrawable);
+                }
+                mTouchingDrawable = NONE;
                 return true;
             }
         } else if (event.getAction() == MotionEvent.ACTION_CANCEL) {
-            if (mWhichDrawableIsDectecting != null) {
-                mWhichDrawableIsDectecting = null;
-                return true;
-            }
-        } else {
-            if (mWhichDrawableIsDectecting != null) {
-                return true;
+            if (mTouchingDrawable != NONE) {
+                mTouchingDrawable = NONE;
+                return false;
             }
         }
 
         return false;
+    }
+
+    private int findTouchingDrawable(MotionEvent event) {
+        int x = (int) event.getX();
+        int y = (int) event.getY();
+
+        Drawable[] drawables = mTextView.getCompoundDrawables();
+
+        for (int i = 0; i < drawables.length; i++) {
+            Drawable drawable = drawables[i];
+
+            if (drawable != null) {
+                Rect rect = drawable.getBounds();
+
+                if (i == 0) {
+                    if (x >= mTextView.getPaddingLeft() && x <= rect.width() + mTextView.getPaddingLeft() &&
+                            y >= (mTextView.getHeight() / 2) - (rect.height() / 2) && y <= (mTextView.getHeight() / 2) + (rect.height() / 2)) {
+                        return i;
+                    }
+                } else if (i == 1) {
+                    if (y >= mTextView.getPaddingTop() && y <= rect.height() + mTextView.getPaddingTop() &&
+                            x >= (mTextView.getWidth() / 2) - (rect.width() / 2) && x <= (mTextView.getWidth() / 2) + (rect.width() / 2)) {
+                        return i;
+                    }
+                } else if (i == 2) {
+                    if (x >= mTextView.getWidth() - rect.width() - mTextView.getPaddingRight() && x <= mTextView.getWidth() - mTextView.getPaddingRight() &&
+                            y >= (mTextView.getHeight() / 2) - (rect.height() / 2) && y <= (mTextView.getHeight() / 2) + (rect.height() / 2)) {
+                        return i;
+                    }
+                } else if (i == 3) {
+                    if (y >= mTextView.getHeight() - rect.height() - mTextView.getPaddingBottom() && y <= mTextView.getHeight() - mTextView.getPaddingBottom() &&
+                            x >= (mTextView.getWidth() / 2) - (rect.width() / 2) && x <= (mTextView.getWidth() / 2) + (rect.width() / 2)) {
+                        return i;
+                    }
+                }
+            }
+        }
+
+        return NONE;
     }
 }
